93e292bfef232c208a78c45de2d0aa8dd3a23a55
Aljiachi <php4u@live.com>
2024-10-14 20:21:06 +0300

    fix-1

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -1,14 +1,9 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
 import MouminAI from "./components/layouts/MouminAI/MouminAI";
-import MyComponent from "./components/chat/MyComponent";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 Builder.registerComponent(MouminAI, {
   name: "MouminAI",
-});
-
-Builder.registerComponent(MyComponent, {
-  name: "chat-screen",
 });
